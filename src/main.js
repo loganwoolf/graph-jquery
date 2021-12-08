154afa09da675010de6bb91451aa8d261546686f
Logan Woolf <loganwoolf@gmail.com>
2021-12-07 21:45:33 -0700

    rename many variables and classes for consistency

--- src/main.js
+++ src/main.js
@@ -2,26 +2,9 @@
 $(function(){
 
   function drawBarChart(data, options, element) {
-    /*
 
-    data is an array of values to be plotted sequentially
-    if nested arrays are detected, a stacked bar will be plotted
-
-    options object includes ...
-     width: total width in px of returned chart
-     height: same as above
-
-     bars:
-     barColor: color of bars
-     barLabelColor: color of labels
-     barLabelPosition: inside the bar at the top, centre, or bottom
-     barWidth: width of bars as % of space available for each
-
-    element is the class given to the div element to be created and appended to <body> that will contain the chart
-
-    */
     $defaultHeight = 600;
-    $structData = [];
+
     // convert data array to objects if not already
     if (typeof data[0] === 'number') {
       $.each(data, function(index, barValue){
@@ -31,19 +14,28 @@ $(function(){
       $structData = data;
     }
 
+    $structuredData = [];
     $chart = $("<div>").addClass('chart');
     $chart.css('display', 'flex');
     $chart.css('align-items', 'flex-end');
     $chart.css('justify-content', 'space-between');
     options.height ? $chart.css('height', options.height + 'px') : $chart.css('height', $defaultHeight);
+
+
+
+    $barsDiv = $('<div>').addClass('bars');
+    $barsDiv.css('display', 'flex');
+    $barsDiv.css('align-items', 'flex-end');
+    $barsDiv.css('justify-content', 'space-between');
+    options.height ? $barsDiv.css('height', options.height + 'px') : $barsDiv.css('height', $defaultHeight);
     // compute scale for chart by dividing chart height by maximum data value
-    $barScale = (options.height || $defaultHeight) / $structData.reduce((acc, barObj) => Math.max(acc, barObj.value), 0);
+    $barScale = (options.height || $defaultHeight) / $structuredData.reduce((acc, barObj) => Math.max(acc, barObj.value), 0);
     // create a bar for each element in the array
-    $.each($structData, function(index, dataObj) {
+    $.each($structuredData, function(index, dataObj) {
       // create bar
-      $bar = $("<div>")
+      $bar = $('<div>')
         .addClass('bar')
-        .attr("data-value", dataObj.value)
+        .attr('data-value', dataObj.value)
         .css('height', (dataObj.value * $barScale) + 'px')
         .css('display', 'flex')
         .css('justify-content', 'center')
@@ -51,7 +43,7 @@ $(function(){
       // set bar options (except label position)
       $bar
         .css('background-color', options.barColor)
-        .css('min-width', (options.barWidth / $structData.length) + '%')
+        .css('min-width', (options.barWidth / $structuredData.length) + '%')
         ;
       //set label container position within bar
       switch(options.labelPosition) {
@@ -66,16 +58,16 @@ $(function(){
         break;
       }
       // create label container in bar
-      $labelContainer = $("<div>")
-        .addClass('label')
+      $barLabelDiv = $('<div>')
+        .addClass('bar-label')
         .css('display', 'flex')
         .css('justify-content', 'center')
         // .css('align-items', 'center')
         ;
       // set label container options (position)
       // create label in label container
-      $label = $("<p>")
-        .addClass('value-label')
+      $barLabelValue = $('<p>')
+        .addClass('bar-label-value')
         .text(dataObj.value)
         .css('margin', '0')
         .css('font-weight', '700')
@@ -84,48 +76,49 @@ $(function(){
         // pad labels depending on flex-align
       switch(options.labelPosition) {
       case 'top':
-        $label.css('margin-top', '3px');
+        $barLabelValue.css('margin-top', '3px');
         break;
       case 'bottom':
-        $label.css('margin-bottom', '3px');
+        $barLabelValue.css('margin-bottom', '3px');
         break;
       }
 
         // idea: make size dependent on digit count
 
         // set label options
-      $label
+      $barLabelValue
         .css('color', options.labelColor)
         .css('font-size', options.labelSize + 'px')
       ;
 
-      $labelContainer.append($label);
-      $bar.append($labelContainer);
+      $barLabelDiv.append($barLabelValue);
+      $bar.append($barLabelDiv);
 
-      $chart.append($bar);
+      $barsDiv.append($bar);
     });
 
     // create a name label for each bar on the x-axis
-    $xAxisLabel = $("<div>")
-      .addClass('x-axis-label')
+    $xAxisDiv = $('<div>')
+      .addClass('x-axis')
       .css('display', 'flex')
       .css('justify-content', 'space-between')
       ;
-    $.each($structData, function (index, dataObj) {
-      $label = $("<p>")
-      .css('width', (options.barWidth / $structData.length) + '%')
+    $.each($structuredData, function (index, dataObj) {
+      $barLabelValue = $('<p>')
+      .css('width', (options.barWidth / $structuredData.length) + '%')
       .css('text-align', 'center')
       .css('color', (options.xAxisColor || 'black'))
       .css('font-size', options.xAxisSize + 'px')
       .css('transform', 'rotate(' + options.xAxisRotation + 'deg)')
       // .css('transform-origin', '%')
       .text(dataObj.name);
-      $xAxisLabel.append($label)
+      $xAxisDiv.append($barLabelValue)
       ;
     });
 
     // create title for chart
     $titleBlock = $('<div>')
+      .addClass('title')
       .css('background-color', options.titleColor[1] || 'hsla(0, 0%, 0%, 0)')
       ;
     $title = $('<h1>')
@@ -139,13 +132,13 @@ $(function(){
     // create
 
     // add items to chartContainer
-    $chartContainer = $("<div>").addClass(element);
+    $chartContainer = $('<div>').addClass('api-output ' + element);
     $chartContainer.append($titleBlock);
-    $chartContainer.append($chart);
-    $chartContainer.append($xAxisLabel);
+    $chartContainer.append($barsDiv);
+    $chartContainer.append($xAxisDiv);
 
     // options for body element
-    $("body")
+    $('body')
       .append($chartContainer)
       .css('font-family', 'monospace')
       .css('background-color', 'grey');
@@ -184,7 +177,7 @@ $(function(){
       xAxisSize: 8,
       xAxisRotation: 315
     },
-    'first-chart');
+    'monthly-change');
 
 });
 
